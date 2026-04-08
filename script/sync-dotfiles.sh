7775dda0aba0d7930238f2af1de5abeac025b052
CanardBizarre <huynhvuongtu@gmail.com>
2026-04-08 02:10:12 +0200

    Initialize nvim script wallpaper zsh

---
+++ script/sync-dotfiles.sh
@@ -0,0 +1,32 @@
+set -euo pipefail
+
+
+CONFIG_DIR="$HOME/.config"
+
+CONFIG_LINKS=(
+	hypr
+	nvim
+	)
+
+
+
+link_item(){
+	local src="$1"
+	local dest="$2"
+
+	if [[ -e "$src" ]];then
+		echo "Linking $src to $dest"
+		ln -sfT "$src" "$dest"
+	else
+		echo "Skipping missing item: $src" >&2
+	fi
+
+}
+
+
+for item in "${CONFIG_LINKS[@]}"; do
+	link_item "$DOTFILES/$item" "$CONFIG_DIR/$item"
+done
+
+link_item "$DOTFILES/zsh/.zshenv" "$HOME/.zshenv"
+link_item "$DOTFILES/git/.gitconfig" "$HOME/.gitconfig"
