5b3eb4f18fe418a4634c8ffb9dc05f4db945521f
CanardBizarre <huynhvuongtu@gmail.com>
2026-05-31 01:37:56 +0200

    xwayland config

---
+++ script/lock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "127.0.0.1 youtube.com" >>/etc/hosts
+echo "127.0.0.1 www.youtube.com" >>/etc/hosts
+echo "127.0.0.1 discord.com" >>/etc/hosts
+echo "127.0.0.1 www.discord.com" >>/etc/hosts
+echo "127.0.0.1 instagram.com" >>/etc/hosts
+echo "127.0.0.1 www.instagram.com" >>/etc/hosts
---
+++ script/unlock.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sed -i '/youtube\|discord\|instagram/d' /etc/hosts
